a3fa34d3625c9b0551378d73c2ad2326ea707976
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-11 00:50:47 +0800

    Create Data Mining.R

---
+++ Data Mining.R
@@ -0,0 +1,10 @@
+# load required packages
+library(dplyr)
+library(tidyr)
+library(car) # for linearHypothesis()
+library(ggplot2) # optional. we expect you to know base graphics, but allow ggplot if you find it easier
+library(psych) # for pairs.panels()
+library(factoextra) # for fviz_cluster()
+library(wooldridge)
+library(kernlab)
+
