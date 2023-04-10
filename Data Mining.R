3c9db54db8f6324f101a6556f44821703d584e1e
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-11 00:51:22 +0800

    Update Data Mining.R

--- Data Mining.R
+++ Data Mining.R
@@ -8,3 +8,17 @@ library(factoextra) # for fviz_cluster()
 library(wooldridge)
 library(kernlab)
 
+#Import the dataset and name it as gpa3
+gpa3<-read.csv("/Users/judyhuang/Desktop/NUS/Business Analyst/Data Mining/dataset/gpa3.csv")
+
+# ----------------------
+#Select independent variables "X"
+#The independent variables in gpa3X are used for part [Q2(b)]
+# ----------------------
+gpa3X = gpa3 %>% select(c("verbmath","hsrank", "hsperc","trmgpa", "tothrs","sat"))
+psych::pairs.panels(gpa3X, lm=TRUE)
+
+# --------------------
+# create a binary variable 'pass',
+# --------------------
+gpa3$pass <- ifelse(gpa3$cumgpa > quantile(gpa3$cumgpa, 0.2), 1, 0)
