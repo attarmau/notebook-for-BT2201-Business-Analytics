5bf60c0796b8730bfdd9d3a969a2a597ae3ad61e
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-11 00:47:46 +0800

    Update Data Exploration.R

--- Data Exploration.R
+++ Data Exploration.R
@@ -8,7 +8,7 @@
 # - Chick: a unique identifier for the chick. 
 # - Diet: a variable from 1, ..., 4 indicating which experimental diet the chick received.
 
-#### 1) Inspect the dataset (2 marks)
+#### 1) Inspect the dataset
 # i) Use the functions you have learnt in Part 1 of this tutorial to inspect the dataset `ChickWeight`. Describe this dataset in 
 #    terms of the number of observations, number of variables, and type of variables. (2 marks)
 ?ChickWeight
@@ -20,14 +20,14 @@ head(ChickWeight)
 tail(ChickWeight)
 
 #### 2) Variables (5 marks)
-# i) Assign the dataset `ChickWeight` to `dfc`. (1 mark)
+# i) Assign the dataset `ChickWeight` to `dfc`.
 dfc <- ChickWeight
 
-# ii) Rename the variable in `dfc` from `weight` to `wtkg`. (1 mark)
+# ii) Rename the variable in `dfc` from `weight` to `wtkg`. 
 dfc$wtkg <- dfc$weight
 dfc$wtkg
 
-# iii) Convert the values in `wtkg` from grams to kilograms [hint: 1 kg = 1000g] (1 mark)
+# iii) Convert the values in `wtkg` from grams to kilograms [hint: 1 kg = 1000g]
 dfc$wtkg <- dfc$wtkg / 1000
 
 # iv) Create a new *factor* variable in `dfc` called `size`. `size` is an ordered factor with two values "Small" and "Large". 
@@ -38,17 +38,17 @@ dfc$size[dfc[, 'wtkg'] < 0.12] <- 'Small'
 dfc$size[dfc[, 'wtkg'] >= 0.12] <- 'Large'
 dfc
 
-#### 3) Data Extraction (6 marks)
+#### 3) Data Extraction
 # i)	Extract the columns `wtkg` and `Time` from `dfc` and assign it to `dfc3`. Export `dfc3` as a csv file. (2 marks)
 dfc3 <- dfc[,'wtkg' & 'time']
 write.csv(dfc3, "dfc3.csv")
 
 # ii) Using Base R functions, extract the rows from `dfc` where `Time` is less than  12 AND `Size` is equal to Large. 
-#      How many rows are extracted? (2 marks)
+#      How many rows are extracted? 
 dfc[dfc$Time < 12 & dfc$Size == 'Large', ]
 
 # iii) Using dplyr functions, remove the `wtkg` and `Diet` columns and retain only the rows where  `Size` is Large, `Time` is 
-#      less than 12 and Diet is 3 or 4. Assign this output to `dfc3iii`. How many observations are there in `dfc3iii`? (2 marks)
+#      less than 12 and Diet is 3 or 4. Assign this output to `dfc3iii`. How many observations are there in `dfc3iii`?
 dfc3iii <- dfc [,-4 & -5]
 dfc3iii <- subset(dfc, select = -c(wtkg & Diet))
 dfc3iii
@@ -60,7 +60,7 @@ count(dfc3iii)
 # (For tutorial discussion: How would you described the rows of observations that were extracted in parts ii and iii?) 
 
 
-#### 4) Sorting (7 marks)
+#### 4) Sorting
 # i)	Using base R, keep only observations where Time is 8 and assign these data to a dataframe dfc4. Sort `dfc4` in increasing 
 #     order of `Size`. Looking at `dfc4`, how many large and small chicks are there at Time=8? (3 marks)
 # ii)	Using dplyr, keep only observation of day 21, then sort the data in decreasing order of `wtkg` followed by increasing order
