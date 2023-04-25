f12f5e4c12c8a3f064e51d67cb8418b61aed6891
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-26 00:58:12 +0800

    Update Data Exploration.R

--- Data Exploration.R
+++ Data Exploration.R
@@ -1,6 +1,16 @@
 library("tidyverse") 
 library("rpivotTable")
 library("knitr")
+
 # import excel file into RStudio
 library(readxl)
 BD <- read.excel("Bank Credit Risk Data.xlsx", sheet = "Base Data", skip = 2)
+
+# example of drawing a histogram
+h.s <- hist (ceosal1$`salary`,
+             main= 'Histogram of salary',
+             xlab = 'salary',
+             ylab = 'Amount of salary',
+             col = c('darkorange'),
+             ylim = c(0,160),
+             labels = TRUE)
