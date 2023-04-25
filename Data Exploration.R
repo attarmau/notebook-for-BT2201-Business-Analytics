1e9962d19905ca7400cc49d5ab9d0908502cbaf4
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-25 23:15:45 +0800

    Update Data Exploration.R

--- Data Exploration.R
+++ Data Exploration.R
@@ -1,1 +1,6 @@
- 
+library("tidyverse") 
+library("rpivotTable")
+library("knitr")
+# import excel file into RStudio
+library(readxl)
+BD <- read.excel("Bank Credit Risk Data.xlsx", sheet = "Base Data", skip = 2)
