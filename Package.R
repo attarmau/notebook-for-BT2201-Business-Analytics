57016b75f975a54f4a5726128386c809cf440614
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-25 21:50:37 +0800

    Create Package.R

---
+++ Package.R
@@ -0,0 +1,19 @@
+# predictive/prescriptive analytics
+library(tseries) 
+library(forecast)
+library(lpSolve)
+library(TTR)
+library(stats)
+
+# descriptive analytics
+library(Rmisc)
+library(rcompanion)
+library(rpivotTable)
+library(car)
+library(rstatix)
+library(psych)
+
+# general use
+library(wooldridge)
+library(knitr)
+library(tidyverse)
