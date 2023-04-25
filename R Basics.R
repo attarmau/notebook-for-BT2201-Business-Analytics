a779bfae2dada831c1dba7aaca7281736c35a549
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-25 21:29:59 +0800

    Update R Basics.R

--- R Basics.R
+++ R Basics.R
@@ -45,3 +45,39 @@ df3.3
 df3.4 <- ToothGrowth
 df3.4$dose.new <- df3.4$dose + 0.1
 df3.4
+
+#  v) Create a new variable `high.dose` and assign it a value of "TRUE" if `dose` is more than 1 and "FALSE" if `dose` is less than or equal to 1. Assign the dataframe with the new variable `high.dose` to df3.5. 
+#     Export df3.5 to a csv file. Discuss what is the r code to export as an excel file (.xlsx). 
+df3.5 <- ToothGrowth
+df3.5$high.dose[df3.5[,'dose'] > 1] <- 'TRUE'
+df3.5$high.dose[df3.5[,'dose'] <= 1] <- 'FALSE'
+df3.5
+
+#### Sorting
+#  i)	There are two functions in Base R “sort” and “order” to perform sorting. How do these two functions differ? 
+#       Try to do a sort with each function on ToothGrowth$len.
+## 'sort' returns the original object, sorted in ascending order by default.
+## 'order' returns the indices of the sorted object, also in ascending order by fault.
+myvector <- c(10,30,15,40)
+sort(myvector)
+order(myvector)
+
+#  ii)	Using a base R function (e.g. order), how can you sort the dataframe `ToothGrowth` in decreasing order of `len`? 
+ToothGrowth[order(ToothGrowth$len, decreasing = TRUE), ]
+
+#  iii) What dplyr functions can you use to sort `ToothGrowth` in increasing order of `len`?
+ToothGrowth %>% arrange(len)
+
+#        Can you also sort the dataframe in decreasing order of `len`?  
+ToothGrowth %>% arrange(desc(len))
+
+#### Factors
+#  i)	Check if `supp` is a factor vector. First type ToothGrowth$supp. What do you observe with the output? 
+ToothGrowth$supp
+
+#  ii)	Next use is.factor() and is.ordered() to check if supp is a factor and if so whether it is an ordered factor. 
+is.factor(ToothGrowth$supp)
+
+#  iii) Now supposed we find that vitamin C (VC) is a superior supplement compared to orange juice (OJ), and we want to order `supp` such that VC is a higher level than OJ, how could we do this? 
+is.ordered(ToothGrowth$supp)
+
