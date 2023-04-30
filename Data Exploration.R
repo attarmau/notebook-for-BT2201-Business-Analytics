e6bad48aa0eeb7a5572de84a08403c63f615a273
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-01 02:13:08 +0800

    Update Data Exploration.R

--- Data Exploration.R
+++ Data Exploration.R
@@ -4,4 +4,4 @@ library("knitr")
 
 # import excel file into RStudio
 library(readxl)
-BD <- read.excel(".xlsx", sheet = "Base Data", skip = 2)
+BD <- read.excel("this is your files name.xlsx", sheet = "Base Data", skip = 2)
