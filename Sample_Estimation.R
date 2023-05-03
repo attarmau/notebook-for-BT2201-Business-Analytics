9d20c2adaab8ad8d83d2d7b797842f799f5b668f
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-03 21:10:01 +0800

    Update Sample_Estimation.R

--- Sample_Estimation.R
+++ Sample_Estimation.R
@@ -1,1 +1,8 @@
-
+library("rcompanion") #this package is required for transformTukey function
+library("rstatix") # for Welch ANOVA test
+library("Rmisc")  # ci function to compute confidence interval
+library("rpivotTable")
+library("knitr")
+library("psych")
+library("dplyr") 
+library("tidyr")
