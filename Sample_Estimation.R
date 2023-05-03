48988d424236e7b9f30fb2232a6d0c67272077ce
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-03 21:13:01 +0800

    Update Sample_Estimation.R

--- Sample_Estimation.R
+++ Sample_Estimation.R
@@ -7,6 +7,8 @@ library("psych")
 library("dplyr") 
 library("tidyr")
 
-- i) compute the 95% and 99% confidence intervals for the mean of `Amount` for DVD sale transactions. Which interval is wider and how does a wider interval affect type 1 error?
-- ii) compute the 90% confidence interval for proportion of DVD sale transactions with sales amount being greater than \$22. Could the company reasonably conclude that the true proportion of DVD sale transactions with sales amount greater than \$22 is 30%? 
-- iii) compute the 95% prediction interval for `Amount` for sales of DVD. Explain to the store manager what this prediction interval mean? 
+# i) compute the 95% and 99% confidence intervals for the mean of `Amount` for DVD sale transactions. Which interval is wider and how does a wider interval affect type 1 error?
+dfD <- ST%>%filter(Product=='DVD')
+plot(density(dfD$Amount), main = 'Density lot for 'Amount' for DVD orders)
+# ii) compute the 90% confidence interval for proportion of DVD sale transactions with sales amount being greater than \$22. Could the company reasonably conclude that the true proportion of DVD sale transactions with sales amount greater than \$22 is 30%? 
+# iii) compute the 95% prediction interval for `Amount` for sales of DVD. Explain to the store manager what this prediction interval mean? 
