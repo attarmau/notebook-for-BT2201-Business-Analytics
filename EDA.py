a4999edd81c9e76eadf20527658bc7a4d9b9920b
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-14 07:47:30 +0800

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -10,3 +10,14 @@ NA_col.plot(kind = 'bar')
 plt.title (' columns having null values')
 plt.ylabel('% null values')
 plt.show()
+
+# Top 10 correlated variables
+df.drop(columns=[''], inplace=True)
+corr = df[df['flag_bad'] == 1].corr()
+corrdf = corr.where(np.triu(np.ones(corr.shape), k=1).astype(np.bool))
+corrdf = corrdf.unstack().reset_index()
+corrdf.columns = ['Var1', 'Var2', 'Correlation']
+corrdf.dropna(subset = ['Correlation'], inplace = True)
+corrdf['Correlation'] = round(corrdf['Correlation'], 2)
+corrdf['Correlation'] = abs(corrdf['Correlation'])
+corrdf.sort_values(by = 'Correlation', ascending = False).head(10)
