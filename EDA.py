7d789c034766bdbbf4973218819adddc92eb9fb7
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-13 18:59:38 +0800

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,3 +2,10 @@ df['put ur column name here'].dtype # dtype('float64')
 df.info()
 df.describe()
 df.isnull().sum() # check missing value
+
+# grafical representation of columns having % null values
+plt.figure(figsize= (20,4),dpi=300)
+NA_col.plot(kind = 'bar')
+plt.title (' columns having null values')
+plt.ylabel('% null values')
+plt.show()
