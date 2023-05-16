805accbffdf93b651513280cdc3dcc84b1769a11
attarmau <87364730+attarmau@users.noreply.github.com>
2023-05-16 13:39:46 +0800

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,8 +2,6 @@ df['put ur column name here'].dtype # dtype('float64')
 df.info()
 df_app.shape # (row number, column number) 
 df.describe()
-df.isnull().sum() # check missing value
-pd.DataFrame(df)
 
 # grafical representation of columns having % null values
 plt.figure(figsize= (20,4),dpi=300)
@@ -25,3 +23,8 @@ corrdf.sort_values(by = 'Correlation', ascending = False).head(10)
 
 my_report = sv.analyze(df)
 my_report.show_notebook()
+
+pd.DataFrame(df)
+df.shape
+df.info()
+df.isnull().sum() # check missing value
