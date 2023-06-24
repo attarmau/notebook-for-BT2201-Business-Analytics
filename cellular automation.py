5322a137e09f74e206f621caf12f005738ceafd5
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-25 00:13:13 +0800

    Update cellular automation.py

--- cellular automation.py
+++ cellular automation.py
@@ -2,10 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def cellular_automaton(rule, seed, t, N):
-    # Define the transition rule as a binary array
-    transition_table = np.array([int(x) for x in np.binary_repr(rule, 8)])
-    # Define the initial seed as a binary array
-    if seed == "single":
+    transition_table = np.array([int(x) for x in np.binary_repr(rule, 8)])     # Define the transition rule as a binary array
+
+    if seed == "single":     # Define the initial seed as a binary array
         initial_seed = np.zeros(N, dtype=int)
         initial_seed[N // 2] = 1
         elif seed == "random":
